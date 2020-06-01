dae8ebe03078654c433d9f5e7e24bd64b7b93e3d
Ruchira Agrawal <ruchira.agrawal@compass.com>
2020-05-31 21:16:21 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+### Conversions + Special Feeds
+#### titan_dataconversionservice-1.46.0
+**Message: [LDCB-81] Enforce string return types for FREE_TEXT_DETAILS (#6980)**
+Commited By: Gábor Varga
+[4950e67a711521001e9e1774c94e27b353b4fd86](https://github.com/UrbanCompass/urbancompass/commit/4950e67a711521001e9e1774c94e27b353b4fd86)
+
+**Files**
+- M	src/python/uc/listing_data_sources/common/converters/listing_details.py
+
+**Message: [LDCO-676] nestio: Fixing a firehose batch request failure issue (#7020)**
+Commited By: reva-compass
+Link{destination=https://github.com/UrbanCompass/urbancompass/commit/d6de054a56f33f0c3b17c47f81c133a680d3c0c2, title=d6de054a56f33f0c3b17c47f81c133a680d3c0c2}
+
+**Files**
+- M	src/python/uc/listing_data_sources/nyc/nestio/nestio_consumer.py
+
+**Message: [LDCB-55] removing temporary log line (#6954)**
+Commited By: reva-compass
+Link{destination=https://github.com/UrbanCompass/urbancompass/commit/a8b075b9a9e0b78fa41634c53a9874a5acbbf292, title=a8b075b9a9e0b78fa41634c53a9874a5acbbf292}
---
+++ src/ChangelogGenerator.java
@@ -0,0 +1,169 @@
+
+import net.steppschuh.markdowngenerator.list.UnorderedList;
+import net.steppschuh.markdowngenerator.text.Text;
+import net.steppschuh.markdowngenerator.text.emphasis.BoldText;
+import net.steppschuh.markdowngenerator.text.heading.Heading;
+import net.steppschuh.markdowngenerator.link.Link;
+import okhttp3.*;
+
+import java.io.*;
+import java.util.*;
+
+import org.json.JSONObject;
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+
+public class ChangelogGenerator {
+
+    public static void main(String args[]) {
+
+        Map<String, List<String>> headerPackageList = new HashMap<>();
+
+//        headerPackageList.put("Syndication", Arrays.asList("src/python/web/controllers/syndication"));
+//        headerPackageList.put("OLR", Arrays.asList("src/python/uc/special_feeds/olr"));
+        headerPackageList.put("Conversions + Special Feeds", Arrays.asList(
+                "/Users/ruchira.agrawal/development/urbancompass/src/python/uc/listing_data_sources",
+                "/Users/ruchira.agrawal/development/urbancompass/src/thrift/urbancompass/listing",
+                "/Users/ruchira.agrawal/development/urbancompass/src/java/com/urbancompass/converter_app",
+                "/Users/ruchira.agrawal/development/urbancompass/src/python/uc/listings_conversion"
+        ));
+
+
+        if (args.length < 1) {
+            return;
+        }
+        String version_old = args[0];
+        String version_new = args[1];
+        headerPackageList.forEach((header, packageList) -> {
+            String packages = "-- " + String.join(" -- ", packageList);
+            String format = "%%h";
+            try {
+
+                String command = String.format("git log %s..%s --decorate --stat --name-status --format=%%s*%%an*%%H %s",
+                        version_old, version_new, packages);
+                System.out.println(command);
+                Process process = Runtime.getRuntime().exec(command, null, new File("/Users/ruchira.agrawal/development/urbancompass"));
+
+                String newReleaseNotes = printMarkdown(header, version_new, process);
+                System.out.println(newReleaseNotes);
+
+                Parser parser = Parser.builder().build();
+                Node document = parser.parse(newReleaseNotes);
+                HtmlRenderer renderer = HtmlRenderer.builder().build();
+                String newReleaseNote =renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+    System.out.println(newReleaseNote);
+                String currentReleaseNotes = getFromWiki();
+                System.out.println(currentReleaseNotes);
+
+                JSONObject page = new JSONObject(currentReleaseNotes);
+                String currentReleaseNote =  page.getJSONObject("body").getJSONObject("storage").getString("value");
+                String updatedReleasetNote = newReleaseNote + currentReleaseNote;
+                page.getJSONObject("body").getJSONObject("storage").put("value", updatedReleasetNote);
+                int currentVersion = page.getJSONObject("version").getInt("number");
+                page.getJSONObject("version").put("number", currentVersion + 1);
+
+
+                writeToWiki(page.toString());
+
+                if (process.waitFor() == 0) {
+                    System.exit(1);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    private static void writeToWiki(String object) throws IOException {
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        System.out.println(object);
+        RequestBody body = RequestBody.create(mediaType, object );
+        Request request = new Request.Builder()
+                .url("https://compass-tech.atlassian.net/wiki/rest/api/content/1405878960")
+                .method("PUT", body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Basic cnVjaGlyYS5hZ3Jhd2FsQGNvbXBhc3MuY29tOnBDWHdQMEM3cWdnWnRkcExLRUJLQUNFRQ==")
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body());
+    }
+
+
+    private static String getFromWiki() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("https://compass-tech.atlassian.net/wiki/rest/api/content/1405878960?expand=body.storage,version").get()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Basic cnVjaGlyYS5hZ3Jhd2FsQGNvbXBhc3MuY29tOnBDWHdQMEM3cWdnWnRkcExLRUJLQUNFRQ==")
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+
+    private static String printMarkdown(String header, String version, Process process) throws IOException {
+        StringBuilder sb = new StringBuilder()
+                .append(new Heading(header + "\n", 3))
+                .append(new Heading(version, 4));
+
+        String line;
+        BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        while((line = error.readLine()) != null) {
+            System.out.println(line  + "\n");
+        }
+        error.close();
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        int i =1;
+        List<String> fileNames =null;
+        while((line = input.readLine()) != null) {
+            if (line != null && !line.isEmpty()) {
+                if (line.startsWith("M\t") || line.startsWith("A\t") || line.startsWith("D\t")) {
+                    fileNames.add(line);
+                } else {
+                    if(fileNames !=null) {
+                        sb.append(new UnorderedList<>(fileNames));
+                        sb.append(new Text("\n"));
+                    }
+                    String[] lines = line.split("\\*");
+                    sb.append(new Text("\n"));
+                    sb.append(new BoldText("Message: " + lines[0]));
+                    sb.append(new Text("\n"));
+                    sb.append(new Text( "Commited By: " + lines[1]));
+                    sb.append(new Text("\n"));
+                    String text = lines[2];
+                    String url = "https://github.com/UrbanCompass/urbancompass/commit/"+lines[2];
+                    sb.append(new Link(text, url));
+                    sb.append(new Text("\n"));
+                }
+            } else {
+                sb.append(new Text("\n"));
+                sb.append(new BoldText( "Files"));
+                sb.append(new Text("\n"));
+                fileNames = new ArrayList<>();
+
+            }
+
+        }
+
+        input.close();
+
+        OutputStream outputStream = process.getOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        printStream.println();
+        printStream.flush();
+        printStream.close();
+
+        return sb.toString();
+    }
+
+}
+
+
